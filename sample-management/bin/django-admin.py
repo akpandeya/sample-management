ff3f675336196f14173577efaf2f250446875b99
pandeya <pandeya@nisem514.local>
2020-07-04 07:54:47 +0200

    Django added

---
+++ sample-management/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/pandeya/MEGA/python/sample-management/sample-management/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
